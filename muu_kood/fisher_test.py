ed53504924cd670e0ec2e8609036adbc612bf901
pilleriinjukk <pilleriinj@gmail.com>
2021-04-30 14:13:59 +0300

    muu abistav kood

--- muu_kood/fisher_test.py
+++ muu_kood/fisher_test.py
@@ -3,7 +3,6 @@ import scipy.stats as stats
 #Nullhüpotees - RNA splaissimise ja geeniekspressiooni puhul on ühe geeni ülekatete suhe mitmega sama
 #kui p-väärtus on väiksem kui 0,05, siis saame nullhüpoteesi kummutada
 
-oddsratio, pvalue = stats.fisher_exact([[1878, 81], [1863, 16]])
-print(oddsratio) # hinnang 95%-usaldusintervall šansside suhetele
+oddsratio, pvalue = stats.fisher_exact([[33511, 7807], [19896, 1701]]) #cs = 50
 print(pvalue) #kas on statistiliselt oluline (statistiline seos)
 
